b1bf28fc965cb45a2323b2f8fc4aecc98fab4713
Jake <jsparling@avvo.com>
2015-02-28 23:03:04 -0800

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title
+    "Rails Starter - #{page_sub_title}"
+  end
+
+  def page_sub_title
+    @page_sub_title
+  end
+
+  def set_page_subtitle(param)
+    @page_sub_title = param
+  end
 end
