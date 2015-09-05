fbd9d262effe1ff57dfad6177177aa3514d70968
Jake <jsparling@avvo.com>
2015-09-05 09:24:01 -0700

    get application helper working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+  def page_title
+    "change in app helper - #{page_sub_title}"
+  end
 
+  def page_sub_title
+    @page_sub_title
+  end
+
+  def set_page_subtitle(param)
+    @page_sub_title = param
+  end
 end
