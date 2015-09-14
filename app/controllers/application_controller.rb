779e31b44f1db3878bdb8451e6a32faab634259b
Jake <jsparling@avvo.com>
2015-09-13 18:01:20 -0700

    improve page title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_subtitle
+
+  def set_subtitle
+    set_page_subtitle controller_name.capitalize
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def page_title
-    "change in app helper - #{page_sub_title}"
+    main = "change in app helper"
+    return main if page_sub_title.nil?
+    "#{main} - #{page_sub_title}"
   end
 
   def page_sub_title
