80a0590a4a2d7984f0fefc3fa561cf6fbb496d78
Jake Sparling <jsparling@avvo.com>
2016-03-20 10:12:32 -0700

    Refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_subtitle
+  before_action :set_sub_title
 
-  def set_subtitle
-    set_page_subtitle controller_name.capitalize
+  def set_sub_title
+    @page_sub_title = controller_name.capitalize
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
 module ApplicationHelper
+  attr_accessor :page_sub_title
+
   def page_title
     main = "change in app helper"
     return main if page_sub_title.nil?
     "#{main} - #{page_sub_title}"
   end
 
-  def page_sub_title
-    @page_sub_title
-  end
+  def bootstrap_class_for(flash_type)
+    type_translation = {
+      :success => "alert-success",
+      :error => "alert-danger",
+      :alert => "alert-warning",
+      :notice => "alert-info",
+    }
 
-  def set_page_subtitle(param)
-    @page_sub_title = param
+    type_translation.fetch(flash_type.to_sym, flash_type.to_s)
   end
 end
